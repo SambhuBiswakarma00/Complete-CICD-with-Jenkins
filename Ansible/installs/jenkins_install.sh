b033f43eb97e303f59572fc429bd01184c366303
Sambhu Biswakarma <biswakarmasambhu@gmail.com>
2024-07-01 17:13:14 +0530

    Update jenkins_install.sh

--- Ansible/installs/jenkins_install.sh
+++ Ansible/installs/jenkins_install.sh
@@ -1,11 +1,22 @@
-#installing jenkings on master
+#!/bin/bash
+
+# Install OpenJDK 17 JRE Headless
+sudo apt install openjdk-17-jre-headless -y
+
+# Download Jenkins GPG key
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+# Add Jenkins repository to package manager sources
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update package manager repositories
 sudo apt-get update
+
+# Install Jenkins
 sudo apt-get install jenkins -y
 
-#installing java
-sudo apt install openjdk-11-jdk -y
+
+
