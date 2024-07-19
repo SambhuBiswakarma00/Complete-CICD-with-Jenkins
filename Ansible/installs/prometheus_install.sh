a297a162be6c61d505eb340305849b7eb885e55a
Sambhu Biswakarma <biswakarmasambhu@gmail.com>
2024-07-19 15:01:44 +0530

    Update prometheus_install.sh

--- Ansible/installs/prometheus_install.sh
+++ Ansible/installs/prometheus_install.sh
@@ -1,4 +1,4 @@
 sudo wget https://github.com/prometheus/prometheus/releases/download/v2.45.6/prometheus-2.45.6.linux-amd64.tar.gz
 tar -xvf prometheus-2.45.6.linux-amd64.tar.gz 
-cd prometheus-2.45.6.linux-amd64
-nohup sudo ./prometheus &> prometheus.log &
+# cd prometheus-2.45.6.linux-amd64
+# nohup sudo ./prometheus &> prometheus.log &
